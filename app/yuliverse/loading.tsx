55fa255d6828af1b6e61295802e7595ecb37b85d
dacooktora <dacooktora@gmail.com>
2026-08-02 03:33:57 +0700

    Create loading.tsx

---
+++ app/yuliverse/loading.tsx
@@ -0,0 +1,10 @@
+export default function Loading() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <div className="text-center">
+        <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-cyan-400 mx-auto mb-4"></div>
+        <p className="text-white text-lg">Loading Yuliverse L2E...</p>
+      </div>
+    </div>
+  )
+}
